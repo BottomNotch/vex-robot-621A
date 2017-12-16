e120dc800452821ff16b11240cd04cfd9d4ec61a
Josiah Atwell <jlatwell@protonmail.com>
2017-12-16 09:47:46 -0500

    write some auton

--- src/auto.c
+++ src/auto.c
@@ -26,7 +26,56 @@
  * The autonomous task may exit, unlike operatorControl() which should never exit. If it does
  * so, the robot will await a switch to another mode or disable/enable cycle.
  */
+void drive(int left, int right) {
+	fbcSetGoal(&driveLFBC, (int)getSensor(driveEncL) + left);
+	fbcSetGoal(&driveRFBC, (int)getSensor(driveEncR) + right);
+}
+
+void mogo(bool intake) {
+	if(intake) {
+		mogoSet(-84);
+		for(int i = 0; i < 1000; i+=20) {
+			if(getSensor(mogoSwitch)) {
+				break;
+			}
+			delay(20);
+		}
+		mogoSet(0);
+	}
+
+	else {
+		mogoSet(84);
+		delay(700);
+		mogoSet(20);
+	}
+
+}
+
+void mogo10ptZone() {
+	intakeMove(INTAKE);
+	delay(250);
+	drive(1200, 1200);
+	mogo(false);
+	delay(2000);
+	fbcSetGoal(&arm1FBC, 650);
+	mogo(true);
+	drive(800, -800);
+	delay(1500);
+	drive(1700, 1700);
+	delay(2000);
+	fbcSetGoal(&arm1FBC, 1600);
+	delay(500);
+	fbcSetGoal(&arm2FBC, 209);
+	delay(1000);
+	fbcSetGoal(&arm1FBC, 650);
+	intakeMove(OUTTAKE);
+	delay(500);
+	mogo(false);
+	drive(-400, -400);
+}
+
 void autonomous() {
+	mogo10ptZone();
 }
 
 
